0f275e451129fa34b1cc918fce94bdac6a64dd49
qing Cheng <qcheng01@scc-wi3.scc.bu.edu>
2024-09-13 11:04:17 -0400

    Qs1-2

--- main_orig.R
+++ main_orig.R
@@ -18,8 +18,7 @@
 #' less_than_zero(c(-1,0,1,2,3,4))
 #' [1] TRUE FALSE FALSE FALSE FALSE FALSE
 less_than_zero <- function(x) {
-    zero <- 0
-    return(x < zero)
+    return(NULL)
 }
 
 #' Evaluate whether the argument is between two numbers
@@ -45,7 +44,7 @@ less_than_zero <- function(x) {
 #' [2,]  TRUE FALSE FALSE
 #' [3,] FALSE FALSE FALSE
 is_between <- function(x, a, b) {
-    return((x > a) & (x < b))
+    return(NULL)
 }
 
 #' Return the values of the input vector that are not NA
@@ -151,34 +150,17 @@ summarize_matrix <- function(x, na.rm=FALSE) {
 
 # ------------ Helper Functions Used By Assignment, You May Ignore ------------
 sample_normal <- function(n, mean=0, sd=1) {
-  set.seed(1337)
-  samples <- rnorm(n, mean=mean, sd=sd)
-  return(samples)
+    return(NULL)
 }
 
 sample_normal_w_missing <- function(n, mean=0, sd=1, missing_frac=0.1) {
-  set.seed(1337)
-  samples <- rnorm(n, mean=mean, sd=sd)
-  missing <- rbinom(length(samples), 1, missing_frac)==1
-  samples[missing] <- NA
-  return(samples)
+    return(NULL)
 }
 
 simulate_gene_expression <- function(num_samples, num_genes) {
-  set.seed(1337)
-  gene_exp <- matrix(
-    rnbinom(num_samples*num_genes, rlnorm(num_genes,meanlog = 3), prob=runif(num_genes)),
-    nrow=num_genes
-  )
-  return(gene_exp)
+    return(NULL)
 }
 
 simulate_gene_expression_w_missing <- function(num_samples, num_genes, missing_frac=0.1) {
-  gene_exp <- simulate_gene_expression(num_samples, num_genes)
-  missing <- matrix(
-    rbinom(num_samples*num_genes, 1, missing_frac)==1,
-    nrow=num_genes
-  )
-  gene_exp[missing] <- NA
-  return(gene_exp)
+    return(NULL)
 }
