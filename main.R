1877ff5c4e054445c6a88d2246a24c053d8e3cc5
qing Cheng <qcheng01@scc-wi2.scc.bu.edu>
2024-09-13 23:05:57 -0400

    finished Qs5-6

--- main.R
+++ main.R
@@ -82,7 +82,7 @@ rm_na <- function(x) {
 #' [1] 1 4 7
 #' 
 row_medians <- function(x) {
-    return(apply(x, MARGIN = 1, FUNC = median))
+    return(apply(x, MARGIN = 1, FUN = median))
 }
 
 #' Evaluate each row of a matrix with a provided function
@@ -107,7 +107,11 @@ row_medians <- function(x) {
 #' summarize_rows(m, mean)
 #' [1] 2 5 8
 summarize_rows <- function(x, fn, na.rm=FALSE) {
-    return(NULL)
+  # if (na.rm) {
+  #   x <- matrix(x[! is.na(x)])
+  # }
+  #   
+  return(apply(x, MARGIN = 1, FUN = fn))
 }
 
 #' Summarize matrix rows into data frame
@@ -146,9 +150,29 @@ summarize_rows <- function(x, fn, na.rm=FALSE) {
 #' 2 -0.01574033 1.026951 -0.04725656 -2.967057 2.571608      112              70      0
 #' 3 -0.09040182 1.027559 -0.02774705 -3.026888 2.353087      130              54      0
 #' 4  0.09518138 1.030461  0.11294781 -3.409049 2.544992       90              72      0
+#' 
 summarize_matrix <- function(x, na.rm=FALSE) {
-    return(NULL)
+    means <- apply(x, MARGIN = 1, FUN = mean)
+    stdevs <- apply(x, MARGIN = 1, FUN = sd)
+    medians <- apply(x, MARGIN = 1, FUN = median)
+    mins <- apply(x, MARGIN = 1, FUN = min)
+    maxes <- apply(x, MARGIN = 1, FUN = max)
+    lt0 <- apply(x, MARGIN = 1, FUN = function(r) {
+        length(r[r < 0])
+    })
+    gt1_lt5s <- apply(x, MARGIN = 1, FUN = function(r) {
+        length(r[r > 1 & r < 5])
+    })
+    is_nas <- apply(x, MARGIN = 1, FUN = function(r) {
+        length(r[is.na(r)])
+    })
+    to_return <- data.frame(mean = means, stdev = stdevs, median = medians, 
+                            min = mins, max = maxes, num_lt_0 = lt0, 
+                            num_btw_1_and_5 = gt1_lt5s, num_na = is_nas)
+    return(to_return)
 }
+a <- matrix(c(1:9), nrow =3 )
+summarize_matrix(a)
 
 # ------------ Helper Functions Used By Assignment, You May Ignore ------------
 sample_normal <- function(n, mean=0, sd=1) {
